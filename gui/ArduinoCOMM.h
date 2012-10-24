9eac1538e82ccd29eadcb0a645fc50b8e6876049
yuwilbur <yuwilbur@7f6f3b01-10c3-48ce-9242-e0f477912f09>
2012-10-24 04:40:41 +0000

    git-svn-id: https://xp-dev.com/svn/GoldenBoot474@46 7f6f3b01-10c3-48ce-9242-e0f477912f09

---
+++ gui/ArduinoCOMM.h
@@ -0,0 +1,18 @@
+#ifndef ARDUINOCOMM
+#define ARDUINOCOMM
+
+#include "GLOBALS.h"
+
+using namespace System;
+using namespace System::IO:Ports;
+
+class ArduinoCOMM{
+public:
+	ArduinoCOMM();
+	~ArduinoCOMM();
+
+private:
+	
+}
+
+#endif
