4781cc9b3fd415f5d7108845886cc46638ea0a97
JulienLaw1991 <JulienLaw1991@7f6f3b01-10c3-48ce-9242-e0f477912f09>
2012-10-25 02:04:29 +0000

    git-svn-id: https://xp-dev.com/svn/GoldenBoot474@56 7f6f3b01-10c3-48ce-9242-e0f477912f09

--- robotAlgorithm/robotAlgorithm/main.cpp
+++ robotAlgorithm/robotAlgorithm/main.cpp
@@ -3,10 +3,13 @@
 #include <stdio.h>
 #include <conio.h>
 #include <vector>
+#include <string>
 
 #include "MovementAlgorithm.h"
 #include "global.h"
 #include "Serial.h"
+
+#include <opencv\cvaux.h>
 #include <opencv2\opencv.hpp>
 #include <opencv\highgui.h>
 #include <opencv\cxcore.h>
@@ -36,72 +39,89 @@ using namespace std;
 
 int main() {
 	Robot robot;
-	Ball ball;
 	Obstacle obstacle;
 	vector<Ball> balls;
+	CSerial serial;
 	//vector<Obstacle> obstacle;
 
-	cout << "Enter the x coordinate of the robot: ";
-	cin >> robot.x;
-	cout << "Enter the y coordinate of the robot: ";
-	cin >> robot.y;
-	cout << "Enter the angle the robot is facing: ";
-	cin >> robot.angle;
-	/*cout << "Enter the x coordinate of the ball: ";
-	cin >> ball.x;
-	cout << "Enter the y coordinate of the ball: ";
-	cin >> ball.y;
-	cout << "Enter the x coordinate of the obstacle: ";
-	cin >> obstacle.x;
-	cout << "Enter the y coordinate of the obstacle: ";
-	cin >> obstacle.y;*/
-
-	int numBalls;
-	cout << "Enter the number of balls: ";
-	cin >> numBalls;
-	balls.resize(numBalls);
-	for(int i = 0; i < balls.size(); i++) {
-		cout << "Coordinate of Ball" << i+1 << ": " << endl;
-		cout << "X: ";
-		cin >> balls[i].x;
-		cout << "Y: ";
-		cin >> balls[i].y;
-		cout << endl;
-		balls[i].rad = 5;
-	}
-
-	MovementAlgorithm algos = MovementAlgorithm(robot, balls);
-	int szMotorsLeft = algos.returnLeftSize();
-	int szMotorsRight = algos.returnRightSize();
-	if(szMotorsRight == szMotorsLeft)
-		cout << "szMotorsRight == szMotorsLeft" << endl;
-	else
-		cout << "szMotorsRight =/= szMotorsLeft" << endl;
-	for(int i = 0; i < szMotorsRight; i++) {
-		cout << "Left Ticks: " << algos.returnLeftMotor()[i] << endl;
-		cout << "Right Ticks: " << algos.returnRightMotor()[i] << endl;
-	}
-	//MovementAlgorithm algo = MovementAlgorithm(robot, ball, obstacle);
-	/*cout << endl << "Robot distance to ball: " << algo.returnBallDist() << endl;
-	cout << "Robot distance to obstacle: " << algo.returnObsDist() << endl;
-	cout << "Obstacle distance to ball: " << algo.returnball2obs() << endl;
-	cout << "Obstacle Range: " << algo.returnObsRange() << endl;
-	cout << "Robot old angle: " << robot.angle << endl;
-	cout << "Robot new angle (facing ball angle): " << algo.returnBotAngle() << endl;
-	if(algo.returnMoveFlag() && !algo.returnTurnFlag()) {
-		cout << "Sending move signal to left and right motors..." << endl;
-		cout << "Right motor value: " << algo.returnRightMotor() << endl;
-		cout << "Left motor value: " << algo.returnLeftMotor() << endl;
-		cout << "Robot will move straight..." << endl;
-	}
-	else {
-		cout << "Robot is still turning ";
-	}*/
-	
-	cout << endl << "Press ESC to exit...";
 	while(1) {
-		char key = getch();
-		if(key == 27)
+		cout << "Enter the x coordinate of the robot: ";
+		cin >> robot.x;
+		cout << "Enter the y coordinate of the robot: ";
+		cin >> robot.y;
+		cout << "Enter the angle the robot is facing: ";
+		cin >> robot.angle;
+		/*cout << "Enter the x coordinate of the ball: ";
+		cin >> ball.x;
+		cout << "Enter the y coordinate of the ball: ";
+		cin >> ball.y;
+		cout << "Enter the x coordinate of the obstacle: ";
+		cin >> obstacle.x;
+		cout << "Enter the y coordinate of the obstacle: ";
+		cin >> obstacle.y;*/
+
+		int numBalls;
+		cout << "Enter the number of balls: ";
+		cin >> numBalls;
+		balls.resize(numBalls);
+		for(int i = 0; i < balls.size(); i++) {
+			cout << "Coordinate of Ball" << i+1 << ": " << endl;
+			cout << "X: ";
+			cin >> balls[i].x;
+			cout << "Y: ";
+			cin >> balls[i].y;
+			cout << endl;
+			balls[i].rad = 5;
+		}
+
+		MovementAlgorithm algos = MovementAlgorithm(robot, balls);
+		int szMotorsLeft = algos.returnLeftSize();
+		int szMotorsRight = algos.returnRightSize();
+		if(szMotorsRight == szMotorsLeft)
+			cout << "szMotorsRight == szMotorsLeft" << endl;
+		else
+			cout << "szMotorsRight =/= szMotorsLeft" << endl;
+		for(int i = 0; i < szMotorsRight; i++) {
+			cout << "Left Ticks: " << algos.returnLeftMotor()[i] << endl;
+			int testLeft = algos.returnLeftMotor()[i];
+			char* leftDigit = new char[5];
+			if(testLeft > 0) {
+				for(int j = 0; j < 5; j++) {
+					leftDigit[j] = (char)(((int)'0')+testLeft%10);
+					testLeft /= 10;
+				}
+			}
+			else {
+				leftDigit[0] = '-';
+				for(int j = 1; j < 5; j++) {
+					leftDigit[j] = (char)(((int)'0')+testLeft%10);
+					testLeft /= 10;
+				}
+			}
+			cout << "Right Ticks: " << algos.returnRightMotor()[i] << endl;
+			int testRight = algos.returnRightMotor()[i];
+			char* rightDigit = new char[5];
+			if(testRight > 0) {
+				for(int j = 0; j < 5; j++) {
+					rightDigit[j] = (char)(((int)'0')+testRight%10);
+					testRight /= 10;
+				}
+			}
+			else {
+				rightDigit[0] = '-';
+				for(int j = 1; j < 5; j++) {
+					rightDigit[j] = (char)(((int)'0')+testRight%10);
+					testRight /= 10;
+				}
+			}
+			if(serial.Open(3, 57600)) {
+				int leftByteSent = serial.SendData(leftDigit, strlen(leftDigit));
+				int rightByteSent = serial.SendData(rightDigit, strlen(rightDigit));
+			}
+			else
+				cout << "ERROR" << endl;
+		}
+		if((cvWaitKey(10) & 255) == 27)
 			break;
 	}
 	return 0;
