518c3d844847a4b8111705f60db02f519a0b4551
JulienLaw1991 <JulienLaw1991@7f6f3b01-10c3-48ce-9242-e0f477912f09>
2012-10-26 18:30:19 +0000

    git-svn-id: https://xp-dev.com/svn/GoldenBoot474@61 7f6f3b01-10c3-48ce-9242-e0f477912f09

---
+++ Arduino Code/readwritetest/test/test.ino
@@ -0,0 +1,40 @@
+//HEADER FILES
+#include <stdio.h>
+#include <stdlib.h>
+#include <Servo.h>
+#include <SoftwareSerial.h>
+
+#define BAUD_RATE 9600
+
+//Functions 
+void setup();
+
+int incomingByte = 0;
+String readString;
+void setup() {
+  Serial.begin(BAUD_RATE);
+}
+
+//_____________________________________________________________________________
+
+
+
+void loop () {
+  char bytes[5];
+  while(Serial.available()) {
+   delay(10);
+   if(Serial.available() == 5) {
+     for(int i = 0; i < 5; i++) {
+      bytes[i] = Serial.read(); 
+     }
+   }
+  }
+  
+  if(readString.length() > 0) {
+   Serial.write(bytes);
+   for(int j = 0; j < 5; j++) {
+     bytes[j] = ' ';
+   }
+  }
+}
+
