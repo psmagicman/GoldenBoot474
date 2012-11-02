06c52c14830cc0175a906165672a65522062e4aa
JulienLaw1991 <JulienLaw1991@7f6f3b01-10c3-48ce-9242-e0f477912f09>
2012-11-02 21:10:18 +0000

    git-svn-id: https://xp-dev.com/svn/GoldenBoot474@101 7f6f3b01-10c3-48ce-9242-e0f477912f09

---
+++ robotAlgorithm/robotAlgorithm/GLOBALS.h
@@ -0,0 +1,43 @@
+#ifndef __GLOBALS__
+#define __GLOBALS__
+
+#define WIDTH	640
+#define HEIGHT	480
+#define FINAL_WIDTH 400
+#define FINAL_HEIGHT 400
+
+#define FT_TO_CM 30.48
+
+#define BAUD_RATE 57600
+
+extern const int _numOfWebcams;
+extern const int calibrationSize;
+
+extern const double _ballRadius;
+extern const double _robotRadius;
+extern const double _obstacleRadius;
+
+struct Coord2D {
+	double x;
+	double y;
+};
+
+struct Robot {
+	double x;
+	double y;
+	double angle;
+};
+
+struct Ball {
+	double x;
+	double y;
+	double rad;
+};
+
+struct Obstacle {
+	double x;
+	double y;
+	double rad;
+};
+
+#endif
