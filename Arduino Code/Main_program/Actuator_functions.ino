e3768dbb8adf8f5617b8fdaaad74f2c726956f0b
yuwilbur <yuwilbur@7f6f3b01-10c3-48ce-9242-e0f477912f09>
2012-11-04 21:50:23 +0000

    git-svn-id: https://xp-dev.com/svn/GoldenBoot474@110 7f6f3b01-10c3-48ce-9242-e0f477912f09

--- Arduino Code/Main_program/Actuator_functions.ino
+++ Arduino Code/Main_program/Actuator_functions.ino
@@ -6,8 +6,10 @@ This sections contains the functions used to activate the Linear actuator ---- T
 #define LOW 0
 #define HIGH 1
 #define PWM_MAX 255
+#define min_value 
+#define max_value
 
-int actuator_length, actuator_length_1;
+int actuator_length;
 
 //Pins needed for the Linear Actuator
 int actuator_pin1 = 12;
@@ -18,6 +20,7 @@ int actuator_input = A7;
 void Actuator_Activate();
 void Actuator_Deactivate();
 void Actuator_Read();
+
 //Function Definition
 void Actuator_Deactivate() //Pushes the rod out  
 {
@@ -32,14 +35,16 @@ void Actuator_Activate() //Pulls the rod in
   
 }
 
-void Actuator_Read() 
+int Actuator_Read() //For testing purposes, reading the values of the actuator 
 {
   actuator_length = analogRead(actuator_input);
   Serial.print('\n');
   Serial.print("Actuator value analog : ");
   Serial.print(actuator_length);
+  return(actuator_length);
 }
 
+/*
 //Setup -- DELETE after Testing
 void setup()
 { Serial.begin(57600);
@@ -47,11 +52,36 @@ pinMode(actuator_pin1, OUTPUT);
 pinMode(actuator_pin2, OUTPUT);
 pinMode(actuator_input, INPUT);
 digitalWrite(actuator_input, HIGH);
-
 delay(500);
 }
-
-//LOOP --DELETE after testing
+*/
+void Kick()
+{ // Reverse 
+  while ((flag1 == 1) || (flag2 == 1))
+  {value_0 = Actuator_Read();
+     if ( value_0 != value_1)
+   { if ( value_0 <= max_value)
+    { Actuator_Activate();
+      value = Actuator_Read();
+    }
+     else
+      flag1 = 0; 
+   
+    if ( value >= min_value)
+    { Actuator_Deactivate();
+      value = Actuator_Read();
+    }
+    else
+     flag2 = 0; 
+  }
+    else 
+     {Serial.print('/n');
+      Serial.print("The Linear actuator is stuck."); 
+      /// Reset if it gets stuck 
+     }
+  }
+}
+/*LOOP --DELETE after testing
 void loop ()
 { char c;
   if(Serial.available() > 0)
@@ -84,3 +114,4 @@ void loop ()
  // Serial.write("DONE");
   
 }
+*/
