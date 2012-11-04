22f0374add2b35c8ec42f090c50c7b872dfebb02
PFernandes <PFernandes@7f6f3b01-10c3-48ce-9242-e0f477912f09>
2012-11-04 22:02:17 +0000

    git-svn-id: https://xp-dev.com/svn/GoldenBoot474@112 7f6f3b01-10c3-48ce-9242-e0f477912f09

---
+++ Arduino Code/Motor PID/SerialComm/SerialComm.ino
@@ -0,0 +1,31 @@
+void serialEvent(){
+        ReadInputs();
+}
+
+  
+void ReadInputs(){
+        if(Serial.available() > 0){
+           char emergency[1];
+           emergency[0] = Serial.peek();
+       
+           if( emergency[0] == 'E'){
+                
+                 while(Serial.available()){
+                   Serial.read();                   
+                 }
+                 state = 0;
+                 enc1_Count=0;
+                 enc2_Count=0;
+                 Reset();
+                 poslist =0;
+                 poslistFlag = 1;
+                 path.clear();
+                 _path.clear();
+                 Serial.write('1');
+           }
+        }
+}
+        
+
+        
+        
